4833220ed3cc9f5c90ac1977e4ab56ec10756b74
Dzheremilz <trempejeremie@gmail.com>
2021-04-27 12:31:27 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import WeatherApp from "./components/WeatherApp"
+import { useState, useEffect } from "react"
+import CityForm from "./components/CityForm"
 
 function App() {
+  const getTheme = () => window.matchMedia?.('(prefers-color-scheme: dark)').matches
+
+  const [darkMode, setDarkMode] = useState(getTheme)
+  const [city, setCity] = useState("Paris")
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.remove('light-theme')
+      document.body.classList.add('dark-theme')
+    } else {
+      document.body.classList.remove('dark-theme')
+      document.body.classList.add('light-theme')
+    }
+  }, [darkMode])
+
   return (
-    <div className="container my-4">
+    <div className="container min-vh-100">
       <h1 className="display-3 text-center mb-4">Météo Actuelle</h1>
-      <WeatherApp />
+      <WeatherApp city={city} />
+      <CityForm setCity={setCity} darkMode={darkMode} />
+      <div className="form-check form-switch mt-3 d-flex justify-content-end">
+        <label className="form-check-label" htmlFor="activate"> Mode Sombre </label>
+        <input className="form-check-input ms-2 mb-3" type="checkbox" id="activate" checked={darkMode} onChange={() => setDarkMode((darkMode) => !darkMode)} />
+      </div>
     </div>
   )
 }
--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -1,7 +1,55 @@
-import React from "react"
+import { useEffect, useState } from "react"
+import Icon from "./Icon"
+import Description from "./Description"
+import Temperature from "./Temperature"
+import Humidity from "./Humidity"
 
-const WeatherApp = () => {
-  return <div></div>
+const WeatherApp = ({ city }) => {
+  const [conditions, setConditions] = useState({})
+  const [description, setDescription] = useState("")
+  const [iconID, setIconID] = useState("")
+  const [location, setLocation] = useState("")
+  const { mainTemp, feelsLike, humidity } = conditions
+
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}&units=metric&lang=fr`
+    fetch(url)
+      .then((response) => {
+        console.log(response)
+        if (!response.ok) {
+          throw new Error("Météo untrouvable")
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setLocation(`${data.name}, ${data.sys.country}`)
+        setConditions({
+          feelsLike: Math.round(data.main.feels_like),
+          mainTemp: Math.round(data.main.temp),
+          humidity: data.main.humidity
+        })
+        setDescription(data.weather[0].description)
+        setIconID(data.weather[0].icon)
+        setLocation(`${data.name}, ${data.sys.country}`)
+      })
+      .catch((error) => {
+        alert(error.message)
+      })
+  }, [city])
+
+  return (
+    <>
+      {!!location && (
+        <section className="text-center">
+          <Icon iconID={iconID} />
+          <h2 className="mb-4">Conditions météo à {location}</h2>
+          <Description description={description} />
+          <Temperature mainTemp={mainTemp} feelsLike={feelsLike} />
+          <Humidity humidity={humidity} />
+        </section>
+      )}
+    </>
+  )
 }
 
 export default WeatherApp
